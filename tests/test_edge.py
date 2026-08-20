5e77aa174fec7d0000d6b3b42125dfd378a6b94b
K.Jitin <ghost050906@gmail.com>
2026-08-20 23:59:50 +0530

    Delete tests directory

--- tests/test_edge.py
+++
@@ -1,5 +0,0 @@
-from edge.validator import EdgeValidator
-def test_normal():
- r=EdgeValidator().validate({'heart_rate':75,'spo2':97,'systolic_bp':120,'diastolic_bp':78,'temperature':36.8,'respiratory_rate':16});assert r.accepted
-def test_impossible():
- r=EdgeValidator().validate({'heart_rate':75,'spo2':101,'systolic_bp':120,'diastolic_bp':78,'temperature':36.8,'respiratory_rate':16});assert not r.accepted
